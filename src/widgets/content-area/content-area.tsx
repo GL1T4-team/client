40090bcc242367374038be9244b07b8e825d6b49
Rustam Aliev <donkodonko583@gmail.com>
2025-09-30 20:02:52 +0500

    add dynamic page titles from routes

--- src/widgets/content-area/content-area.tsx
+++ src/widgets/content-area/content-area.tsx
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Container, Title } from "./styles";
+import { ROUTES } from "@/shared/constants";
+
+const TITLES: Record<string, string> = {
+    [ROUTES.DASHBOARD]: "Дашборд",
+    [ROUTES.CREATE_REPORT]: "Создать отчет",
+    [ROUTES.UPLOADS]: "Загрузки",
+    [ROUTES.SETTINGS]: "Настройки",
+};
 
 const ContentArea = () => {
+    const location = useLocation();
+    const title = TITLES[location.pathname] || "Главная";
+
     return (
         <Container>
-            <Title>Дашборд</Title>
+            <Title>{title}</Title>
             <Outlet />
         </Container>
     );
