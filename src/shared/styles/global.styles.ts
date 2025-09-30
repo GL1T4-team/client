aac7466c2a4a6beae50de18a7295c45840f651da
Rustam Aliev <donkodonko583@gmail.com>
2025-09-30 14:36:01 +0500

    add global styles

---
+++ src/shared/styles/global.styles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+    }
+    
+    body {
+        font-family: Helvetica, sans-serif;
+        height: 100vh;
+    }`;
+
+export default GlobalStyle;
