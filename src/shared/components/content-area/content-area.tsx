5eeb1c0e192ef64ea2f8a7846f0d53bac9874b0d
Rustam Aliev <donkodonko583@gmail.com>
2025-09-30 19:01:27 +0500

    add content area component

---
+++ src/shared/components/content-area/content-area.tsx
@@ -0,0 +1,12 @@
+import { Container, Title } from "./styles";
+
+const ContentArea = () => {
+    return (
+        <Container>
+            <Title>Дашборд</Title>
+            <div>Контент</div>
+        </Container>
+    );
+};
+
+export default ContentArea;
---
+++ src/shared/components/content-area/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding: 5px 0 0 64px;
+`;
+
+export const Title = styled.div`
+    font-size: 48px;
+    color: #0a0909;
+`;
