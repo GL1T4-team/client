a9d41a2340160560d89780f2af839c76d0dcc021
Rustam Aliev <donkodonko583@gmail.com>
2025-10-03 01:40:26 +0500

    add dashboard page

--- src/pages/dashboard-page/dashboard-page.tsx
+++ src/pages/dashboard-page/dashboard-page.tsx
@@ -1,5 +1,20 @@
+import { Container, Widget } from "./style";
+
 const DashboardPage = () => {
-    return <div></div>;
+    return <Container>
+        <Widget>
+            Средняя длительность полета
+        </Widget>
+        <Widget>
+            Число полетов
+        </Widget>
+        <Widget>
+            Топ-10 регионов
+        </Widget>
+        <Widget>
+            Карта
+        </Widget>
+    </Container>;
 };
 
 export default DashboardPage;
